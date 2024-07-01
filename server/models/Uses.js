6534b0df73fe13dab31d0be5477c16fa1637dc85
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-07-01 11:57:57 +0600

    user router

---
+++ server/models/Uses.js
@@ -0,0 +1,25 @@
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("uses", schema);
